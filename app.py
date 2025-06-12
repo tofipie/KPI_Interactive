4b68b9319c718d53b62295f6fc588e34a5ef4aac
tofipie <62646269+tofipie@users.noreply.github.com>
2025-06-12 13:45:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,9 @@ button = st.button("חשב")
 if user_input1 and user_input2 and button:
     emb1 = model.encode(user_input1)
     emb2 = model.encode(user_input2)
-    similarity_score = round(1 - spatial.distance.cosine(emb1, emb2),2)
-    st.write(f"Similarity Score: {similarity_score}") 
+    similarity_score = 1 - spatial.distance.cosine(emb1, emb2)
+    st.write(f"Similarity Score: {{:.3f}".format(similarity_score)}") 
+
 
 
 files = ['purchase_orders','goods_receipts','vendor_invoices',
